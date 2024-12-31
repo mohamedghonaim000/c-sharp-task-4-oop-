efaaa7b73697de64332bb756b577f3f64ff29895
mohamedghonaim000 <mohamedmokhtar64232@gmail.com>
2024-12-31 13:55:02 +0200

    demo commit

---
+++ demo/Complex.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last_session_oop
+{
+    internal class Complex
+    {
+        public int real {  get; set; }
+        public int imag { get; set; }
+
+        #region overloading binary operator (+ , -)
+        public static Complex operator +(Complex left, Complex right)
+        {
+            return new Complex()
+            {
+                real = (left?.real?? 0) + (right?.real??0),
+                imag = (left?.imag??0) + (right?.imag??0)
+            };
+        }
+        public static Complex operator -(Complex left, Complex right)
+        {
+            return new Complex()
+            {
+                real = left.real - right.real,
+                imag = left.imag - right.imag,
+            };
+        }
+        #endregion
+
+        #region overloading unary operator
+        public static Complex operator ++(Complex c)
+        {
+
+            return new Complex()
+            {
+                real = c.real + 1,
+                imag = c.imag + 1
+            };
+        }
+        public static Complex operator --(Complex c)
+        {
+
+            return new Complex()
+            {
+                real = c.real - 1,
+                imag = c.imag - 1
+            };
+        }
+        #endregion
+
+        #region overloading (< ,> , == , != )
+        public static bool operator >(Complex left, Complex right)
+        {
+            return left.real > right.real;
+        }
+        public static bool operator <(Complex left, Complex right)
+        {
+            return left.real > right.real;
+        }
+
+        public static bool operator == (Complex left, Complex right)
+        {
+            return left.real == right.real;
+        }
+
+        public static bool operator !=(Complex left, Complex right)
+        {
+            return left.real == right.real;
+        }
+        #endregion
+
+        #region casting overloading
+        public static implicit operator Complex(int num)
+        {
+            return new Complex() { imag = num, real = num };
+        }
+        public static explicit operator int(Complex c)
+        {
+            return c.real;
+        } 
+        #endregion
+
+
+
+
+        public override string ToString()
+        {
+            return $"real : {real} imag : {imag}";
+        }
+    }
+}
---
+++ demo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Last_session_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex() { real = 1, imag = 2 };
+            Complex c2 = new Complex() { real = 2, imag = 3 };
+
+            #region + -
+            //Complex c3 =++ c1 ;
+            //Complex c3 = c1++;
+
+            //Console.WriteLine(c3);
+            //Console.WriteLine(c1);
+
+            #endregion
+
+            #region < , > ,== ,!=
+            //if (c2 == c2)
+            //{
+            //    Console.WriteLine("iam here");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("noooooooooooooooooooo");
+            //}; 
+            #endregion
+
+            #region Casting
+            //c1 = c2 + 5;
+            //Console.WriteLine(c1);
+
+            //int y = (int)c2 + 1;
+            //Console.WriteLine(y); 
+            #endregion
+        }
+    }
+}
